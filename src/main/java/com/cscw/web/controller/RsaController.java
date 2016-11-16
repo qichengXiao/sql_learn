94fcdc4249ba850787671e1401a4cbba858b31f7
qichengXiao <574929202@qq.com>
2016-11-17 00:47:43 +0800

    完成RSA算法

--- src/main/java/com/cscw/web/controller/RsaController.java
+++ src/main/java/com/cscw/web/controller/RsaController.java
@@ -33,65 +33,57 @@ import com.huisa.common.exception.ServiceException;
 
 
 @Controller
-@RequestMapping(value = "/playfair")
+@RequestMapping(value = "/rsa")
 public class RsaController {
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String login(Model model) {
-	    //生成两个100-200的素数
+	   
 	    int p ;
 	    int q ; 
-	    int n ,n2,d,e=3;
+	    int n ,n2,d,e=3533;
 	    
 	    boolean flag=false;
-	    do{
-	    	p= 	(int)(Math.random())*100;
-	    	 flag =false;
-	    	for(int i=2;i<p;i++){
-				if(p%i==0){
-					flag = true;
-				}
-			}
-	    }while(flag==false);
-	    
-	    do{
-	    	q= 	(int)(Math.random())*100;
-	    	 flag =false;
-	    	for(int i=2;i<q;i++){
-				if(q%i==0){
-					flag = true;
-				}
-			}
-	    }while(flag==false);
-	   
+	    //生成p、q  两个100-200的素数
+	    p=createSuShu();
+	    q=createSuShu();
+	   //计算n
 	    n=p*q;
+	    //计算φ(n)
 	    n2=(p-1)*(q-1);
-	    
+	    //求d
 	    int i = 1;
-	    int t;
-	    do{
-			   d=(n*i+1)/e;
-			   i++;
-			  t =(d*e)%n;
-		  }while(t==1);
-		   System.out.println("公钥e:"+ e +"n:"+n);
-		   System.out.println("秘钥d:"+ d +"n:"+n);
-	
-		return "password/playfair";
+	    while( (n2*i+1)%e!=0){
+	    	i++;
+	    }
+	    d=(n2*i+1)/e;
+	    System.out.println("公钥e:"+ e +"n:"+n);
+		System.out.println("秘钥d:"+ d +"n:"+n);
+		model.addAttribute("e",e);
+		model.addAttribute("n",n);
+		model.addAttribute("d",d);
+		model.addAttribute("p",p);
+		model.addAttribute("q",q);
+		
+		return "password/rsa";
 	}
 
 	
-	//decrypt
-	@Autowired(required = false)
+	
+	
+	/*
+	 * 加密
+	 */
 	@RequestMapping(value = "/encrypt")
 	@ResponseBody
-	public void count(Model model) {
+	public void encrypt(Model model) {
 		int e = MVCUtil.getIntParam("e");
 		int n = MVCUtil.getIntParam("n");
 		String mingwen = MVCUtil.getParam("mingwen");
 		AjaxData ajaxdata;
 		String msg = null;
+		//验证
 		if(n==0||e==0){
 			msg = "请正确输出公钥e、n	";
 		}
@@ -105,41 +97,42 @@ public class RsaController {
 	           MVCUtil.ajaxJson(ajaxdata);
 	           return;
 	    }
-	    char[] arrayc = mingwen.toLowerCase().toCharArray();
-	   
-	    int[] result = new int [arrayc.length];
+	    char[] arrayc = mingwen.toCharArray();
+	    int niwenNum ;
+	    //加密
+	    StringBuffer miwen = new StringBuffer(50);
 	    for(int i = 0 ;i<arrayc.length;i++){
-	    	int num = arrayc[i];
-	    	int a = (int) (Math.pow(num, e));
-	    	result[i]=( a  )%n;
+	    	niwenNum=modCount(arrayc[i], e, n);
+	    	//规整为5位的字符串添加到字符串miwen中
+	    	translateToFiveChar(miwen,niwenNum);
 	    }
-	     
-	    o.addProperty("mingwen", new String (getPlain(miwen.toCharArray(), juzhen)));
-		o.addProperty("juzhen", juzhenString.toString());
-		o.addProperty("miwen",new String(miwen));
+	    
+	    JsonObject o  = new JsonObject();
+	    o.addProperty("miwen", miwen.toString());
+		o.addProperty("msg", "注意：密文是以五个字符单位的，一个明文字符转换一个五位的整型");
 		ajaxdata = new AjaxData(true, o, null);
 		MVCUtil.ajaxJson(ajaxdata);
-		
-	    //采用一个字符一个分组，这样能保证不会超出范围
-	    
-
+		return;
 	}
 	
+	/*
+	 * 解密
+	 */
 	@RequestMapping(value = "/decrypt")
 	@ResponseBody
-	public void decrypt() {
-		String keyword = MVCUtil.getParam("keyword");
+	public void decrypt(Model model) {
+		int d = MVCUtil.getIntParam("d");
+		int n = MVCUtil.getIntParam("n");
 		String miwen = MVCUtil.getParam("miwen");
 		AjaxData ajaxdata;
 		String msg = null;
-		if(keyword.length()!=8){
-			msg = "输入口令长度必须为8	";
+		//验证
+		if(n==0||d==0){
+			msg = "请正确输出私钥d、n	";
 		}
-		if(keyword.contains("i")&&keyword.contains("j")	){
-			msg="口令不能同时含有ij";
-		}
+		
 		if(!StringUtils.isNotBlank(miwen)){
-			msg = "输入明文不能为空";
+			msg = "输入密文不能为空";
 					
 		}
 		if (StringUtils.isNotBlank(msg)) {
@@ -147,87 +140,89 @@ public class RsaController {
 	           MVCUtil.ajaxJson(ajaxdata);
 	           return;
 	    }
-		//将j替换成i
-		if(keyword.contains("j")){
-			keyword=keyword.replaceAll("j", "i");
-		}
-
-		//生成矩阵
-		char[][] juzhen = new char[5][5];
-		char[] ca = keyword.toCharArray();
-		for(int i = 0;i<8;i++){
-			if(i<5)
-				juzhen[0][i]=ca[i];
-			else
-				juzhen[1][i-5]=ca[i];
-		}
-		
-		// 填充剩下的值
-		int t = 7;
-		for (char i = 'a'; i <= 'z'; i++, t++) {
-			if (i == 'j')
-				continue;
-			fill(juzhen, i, t);
-		}
-		
-
-		JsonObject o = new JsonObject();
-		
-		StringBuffer	juzhenString = new StringBuffer() ;
-		for(char chararray[] :juzhen){
-			for(char c :chararray){
-				juzhenString.append(" ");
-				juzhenString.append(c);
-			}
-			juzhenString.append("\n");
-		}
-		
-		
-		o.addProperty("mingwen", new String (getPlain(miwen.toCharArray(), juzhen)));
-		o.addProperty("juzhen", juzhenString.toString());
-		o.addProperty("miwen",new String(miwen));
+	    char[] arrayc = miwen.toCharArray();
+	    StringBuffer mingwen = new StringBuffer(50);
+	    //以五个字符为单位
+	    for(int i=0;i<arrayc.length;i=i+5){
+	    	//转换成整形
+	    	String s = ""+arrayc[i]+ arrayc[i+1]+ arrayc[i+2]+ arrayc[i+3]+ arrayc[i+4];
+	    	int mi_num = Integer.parseInt(s);
+	    	//解密
+	    	char c = (char)modCount(mi_num, d, n);
+	    	mingwen.append(c);
+	    }
+	    
+	   
+	    
+	    JsonObject o  = new JsonObject();
+	    o.addProperty("mingwen", mingwen.toString());
+		o.addProperty("msg", "注意：密文是以五个字符单位的，一个明文字符转换一个五位的整型");
 		ajaxdata = new AjaxData(true, o, null);
 		MVCUtil.ajaxJson(ajaxdata);
+
 	}
-	@Test
-	public void test(){
-			int p=101 ;
-		    int q=113 ; 
-		    int n ,n2,d,e=3;
-		    
-		    boolean flag=false;
-//		    do{
-//		    	p= 	(int)(Math.random())*100;
-//		    	 flag =false;
-//		    	for(int i=2;i<p;i++){
-//					if(p%i==0){
-//						flag = true;
-//					}
-//				}
-//		    }while(flag==false);
-//		    
-//		    do{
-//		    	q= 	(int)(Math.random())*100;
-//		    	 flag =false;
-//		    	for(int i=2;i<q;i++){
-//					if(q%i==0){
-//						flag = true;
-//					}
-//				}
-//		    }while(flag==false);
-		   
-		    n=p*q;
-		    n2=(p-1)*(q-1);
-		    
-		    int i = 1;
-		    int t ;
-		   do{
-			   d=(n*i+1)/e;
-			   i++;
-			  t =(d*e)%n;
-		   }while(t==1);
-			
-		   System.out.println("e:"+ e +"n:"+n);
-		   System.out.println("d:"+ d +"n:"+n);
+	
+	
+	/*
+	 * 生成素数
+	 */
+	private int createSuShu(){
+		int p ;
+		 boolean flag=false;
+		    do{
+		    	p= 	(int)(Math.random()*100)+100;
+		    	 flag =true;
+		    	for(int i=2;i<p;i++){
+					if(p%i==0){
+						flag = false;
+						break;
+					}
+				}
+		    }while(flag==false);
+		  return p;
 	}
+	
+	
+	/*
+	 * 规定每位密文 占5个字符（因为p、q最大200 n最大40000，最多5位，所求的密文肯定小于n）
+	 */
+	
+	private void translateToFiveChar(StringBuffer s ,int num){
+		 String str = String.valueOf(num);
+		 int length = 5-str.length();
+		 for(int i = 0 ; i<length ; i ++ ){
+	       s.append("0");
+		 }
+		 s.append(str);
+	}
+
+	/*
+	 * 模指运算  
+	 * @param a 明文或密文
+	 * @param ed 明文为e 密文为d
+	 * @param n  n
+	 */
+
+	 private int modCount(int a, int ed, int n) {
+	        if (ed == 0) {
+	            return 1;
+	        }
+	        int r = a % n;
+	        int k = 1;
+	        while (ed > 1) {
+	            if ((ed & 1) != 0) {
+	                k = (k * r) % n;
+	            }
+	            r = (r * r) % n;
+	            ed >>= 1;
+	        }
+	        return (r * k) % n;
+	    }
+	 
+	 @Test
+	 public void test(){
+		 int mi = modCount('a', 3533, 11413);
+		 int ming = modCount(mi, 6597, 11413);
+		 System.out.println(mi+""+(char)ming);
+	 }
 }
